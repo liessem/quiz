31916af0d4fdcd0c08522f8a5d812d84c246367f
Aurélio Gregorio Liessem <aurelio@liessem.com.br>
2018-05-05 09:55:45 -0300

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Aurélio Gregorio Liessem on 05/05/2018.
+//  Copyright © 2018 Aurélio Gregorio Liessem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
